6264f8a4f6505e7e94a786101a034296185f80cc
harute <blurry_alpha@hotmail.com>
2015-07-26 03:04:28 +0900

    複数Activity対応

--- app/src/main/java/com/test/helloworld/SubActivity.java
+++ app/src/main/java/com/test/helloworld/SubActivity.java
@@ -6,38 +6,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class SubActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
 
-        // Button挙動
-        Button button = (Button)findViewById(R.id.buttonpush);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i("test", "test message");
-                Intent intent = new Intent(
-                        MainActivity.this,
-                        SubActivity.class
-                );
-                intent.putExtra("testVal", "abc");
-                startActivity(intent);
-
-            }
-        });
+        Intent intent = getIntent();
+        String strData = (String)intent.getStringExtra("testVal");
+        Log.i("test", strData + " OK!");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_sub, menu);
         return true;
     }
 
